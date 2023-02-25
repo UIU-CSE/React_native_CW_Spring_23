802fee6a63adece95da7a0d04aa53c8deffabad2
MD Ashiqur Rahman Joy <43420293+XLR8-07@users.noreply.github.com>
2023-02-25 10:16:17 +0600

    FlatList Introduced

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./src/Screens/HomeScreen";
+import ListScreen from "./src/Screens/ListScreen";
 
 const stack = createStackNavigator();
 
@@ -10,6 +11,7 @@ function App() {
     <NavigationContainer>
       <stack.Navigator>
         <stack.Screen name="Home" component={HomeScreen} />
+        <stack.Screen name="List" component={ListScreen} />
       </stack.Navigator>
     </NavigationContainer>
   );
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 import SafeAreaView from "react-native-safe-area-view";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View>
         <Text style={{ fontSize: 40 }}>Hello World</Text>
+        <Button
+          title="Go To Another Page"
+          onPress={() => {
+            props.navigation.navigate("List");
+          }}
+        />
       </View>
     </SafeAreaView>
   );
